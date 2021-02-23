cdfbdbb081a0c1e1f59518f4fca1eeef2a9ad8b7
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-02-23 20:55:27 +0300

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array=0) {
+  if(array == 0){
+    return 0;
+  }else{
+    return Math.min.apply(null, array);
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array=0) {
+  if(array == 0){
+    return 0;
+  }else{
+    return Math.max.apply(null, array);
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array=0) {
+  if(array == 0){
+    return 0;
+  }else{
+    let summ = 0;
+    for(i=0; i<array.length; i++){
+      summ += array[i];
+    }
+    return summ/array.length;
+  }
 }
